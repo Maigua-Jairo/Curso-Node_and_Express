f840e87bbb1a8d768698c12374db76763d75615c
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-03-23 20:41:25 -0500

    Node-Express change 2

--- server.js
+++ server.js
@@ -6,20 +6,13 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); // Para pruebas de FCC
-
-// 1. MIDDLEWARES
+fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2. CONFIGURACIÓN DE PUG
-app.set('view engine', 'pug');
-app.set('views', './views/pug'); 
-
-// 3. RUTAS
 app.route('/').get((req, res) => {
-  res.render('index'); 
+
 });
 
 const PORT = process.env.PORT || 3000;
